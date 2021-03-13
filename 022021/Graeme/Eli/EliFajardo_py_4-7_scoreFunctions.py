0d9b29c63a8c8ecbb6f3606023f66bef32fd9e36
EliFajardoB <efajardo.br@gmail.com>
2021-03-13 17:47:50 -0600

    Modify the function to one task

--- 022021/Graeme/Eli/EliFajardo_py_4-7_scoreFunctions.py
+++ 022021/Graeme/Eli/EliFajardo_py_4-7_scoreFunctions.py
@@ -1,34 +1,33 @@
-print ("::: EASY GRADES :::")
+#This program return grades based on given scores
 
-score = 1
+# F U N C T  I O N S
 def computegrade (score):
-    #INPUT SCORE
-    score = input ("Enter score: ")
-    try:
-        score = float(score)
-    except:
-        score = -1 
-        print ("Try input numbers")
-        exit(score)
-    #RETURN SCORE
+    #RETURN GRADE
     if 0 <= score <= 1:
             if score >= 0.9:
-                a = "A"
-                return(a)
+                return("A")
             elif score >= 0.8:
-                b = "B"
-                return (b)
+                return ("B")
             elif score >= 0.7:
-                c = "C"
-                return (c)
+                return ("C")
             elif score >= 0.6:
-                d = "D"
-                return (d)
+                return ("D")
             elif score < 0.6:
-                f = "F"
-                return (F)
+                return ("F")
     else:
         print ("Score is out of range, try again")
+
+print ("::: EASY GRADES :::")
+
+#INPUT SCORE
+score = 1
+score = input ("Enter score: ")
+try:
+    score = float(score)
+except:
+    score = -1 
+    print ("Try input numbers")
+    exit(score)
 
 grade = computegrade(score)
 print ("Grade:", grade)
